c6bcfe85aca47aba7dcbf55d00f0c315ab1047bd
Scott Reeve <info@freedops.org>
2014-05-13 16:54:29 -0400

    add file headers

--- Python/Props_man/test_plain_pickle.py
+++ Python/Props_man/test_plain_pickle.py
@@ -1,7 +1,26 @@
 '''
 Created on 9 Apr 2014
 
-@author: scott
+@author: Scott Reeve
+
+Copyright 2014 freedops.org
+
+This file is part of the freedops.org tools collection.
+
+The freedops.org tools collection is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+The freedops.org tools collection is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the freedops.org tools collection.
+
+If not, see <http://www.gnu.org/licenses/>.
 '''
 import unittest
 import plain_pickle
